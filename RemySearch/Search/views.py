a078fb9fe72871cd7e69a882be8af5d93c8f7db3
GinoGinocchio <gino.ginocchio@unmsm.edu.pe>
2023-06-09 16:03:15 -0500

    Mejorando el Home

--- RemySearch/Search/views.py
+++ RemySearch/Search/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Fino")
-
 def home(request):
     return render (request, 'Search/Home.html')
