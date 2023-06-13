8d25b4da8553f4735dd3ec6989eba3776c4808df
GinoGinocchio <gino.ginocchio@unmsm.edu.pe>
2023-06-13 17:10:16 -0500

    expedientes vista v1.0

--- RemySearch/Search/urls.py
+++ RemySearch/Search/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import *
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('expedientes/', home, name='home'),
+    path('', expediente, name='expediente'),
 ]
 
--- RemySearch/Search/views.py
+++ RemySearch/Search/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render (request, 'Search/Home.html')
+
+def expediente(request):
+    return render (request,'Search/exp.html')
