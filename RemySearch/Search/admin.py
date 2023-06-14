67e1c342db6eb5dcae073e60cb1b4d629a53bd4c
GinoGinocchio <gino.ginocchio@unmsm.edu.pe>
2023-06-13 23:47:05 -0500

    migraciones y modelo

--- RemySearch/Search/admin.py
+++ RemySearch/Search/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class ArchivoPDFAdmin(admin.ModelAdmin):
+    list_display = ['id', 'archivo']
+    search_fields = ['id']
+
+admin.site.register(ArchivoPDF, ArchivoPDFAdmin)
--- RemySearch/Search/models.py
+++ RemySearch/Search/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class ArchivoPDF(models.Model):
+    id = models.AutoField(primary_key=True)
+    archivo = models.FileField(upload_to='pdf/')
