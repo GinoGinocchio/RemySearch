62ceb24834ac86d5eaf68cbbae83f6ff39775ec1
GinoGinocchio <gino.ginocchio@unmsm.edu.pe>
2023-06-16 01:39:29 -0500

    Añadiendo rutas y mejorando front

--- RemySearch/Search/urls.py
+++ RemySearch/Search/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('expedientes/', home, name='home'),
-    path('', expediente, name='expediente'),
+    path('', home, name='home'),
+    path('expedientes/', expediente, name='expediente'),
     path('media/pdf/<pk>/', pdf_view, name='pdf_view'),
 ]
 
