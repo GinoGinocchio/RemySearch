823ce5397557890093e7942db8585cbb6538948e
EGH2019 <elian.gomez@unmsm.edu.pe>
2023-06-27 00:22:38 -0500

    cambios views y models comentados

--- RemySearch/RemySearch/settings.py
+++ RemySearch/RemySearch/settings.py
@@ -51,8 +51,9 @@ HAYSTACK_CONNECTIONS = {
 }
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 
-SOLR_SERVER = 'http://localhost:8983/solr/'
+SOLR_SERVER = 'http://localhost:8983/solr/mincetur'
 SOLR_CONNECTION = pysolr.Solr(SOLR_SERVER)
+SOLR_CORE = 'mincetur'
 
 
 MIDDLEWARE = [
--- RemySearch/Search/views.py
+++ RemySearch/Search/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import get_object_or_404
 from django.conf import settings
 from Search.models import *
 import os
+# import pysolr
+# from tika import parser
 
 def home(request):
     return render (request, 'Search/Home.html')
@@ -13,4 +15,18 @@ def expediente(request):
     objetos = ArchivoPDF.objects.all()
     context['objetos'] = objetos
     return render (request, 'Search/exp.html', context = context)
+
+# def index_pdf_to_solr(pdf_path, document_id):
+#     solr = pysolr.Solr(os.environ['SOLR_SERVER'])
+
+#     # Extraer el contenido del PDF utilizando Tika
+#     parsed_pdf = parser.from_file(pdf_path)
+#     content = parsed_pdf['content']
+
+#     # Preparar los datos para enviar a Solr
+#     doc = {
+#         'id': document_id,
+#         'content': content
+#         # Agrega otros campos según tu esquema de Solr
+#     }
 
